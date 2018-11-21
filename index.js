dd27c4ee1177bf5a4610e264dc27c87db1207314
Travis Martin <travisemerymartin@gmail.com>
2018-11-21 18:49:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
   var unit = [{itemName: `${item}`, itemPrice: Math.floor(Math.random()*100)+1}]
-  cart.concat(unit)
+  cart.push(unit)
   return cart
 }
 
