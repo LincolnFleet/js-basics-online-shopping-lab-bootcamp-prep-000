0cb2a3d6ac4c8673be5f6caf59f6f87b214a7da8
Travis Martin <travisemerymartin@gmail.com>
2018-11-21 19:46:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function viewCart() {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
   } else {
     var currentCart = "In your cart, you have";
-    for (i=0; i<cart.length-1; i++) {
+    for (var i=0; i<cart.length-1; i++) {
       currentCart += ` ${cart[i].itemName} at $${cart[i].itemPrice},`
     }
     currentCart += ` and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`
